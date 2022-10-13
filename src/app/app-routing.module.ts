9b28a8944fc274a694989296c0fd02a7b72ad2e8
Rafarel <rafarel007@hotmail.com>
2022-10-13 10:13:16 +0200

    Add titles to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {ContactComponent} from "./components/contact/contact.component";
 
 const routes: Routes = [
-  { path: "", component: HomeComponent},
-  { path: "contactez-nous", component: ContactComponent},
+    {path: "", title: "Titre accueil", component: HomeComponent},
+    {path: "contactez-nous", title: "Contactez nous", component: ContactComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledBlocking'
-})],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {
+        initialNavigation: 'enabledBlocking'
+    })],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
