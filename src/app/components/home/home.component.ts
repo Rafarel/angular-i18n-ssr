2dcd3aab71eaaf86c4be22825f4d70d5f1d75cc8
Rafarel <rafarel007@hotmail.com>
2022-10-17 16:48:47 +0200

    Test locale injection in constructor

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
     selector: 'app-home',
     template: `
+        <p>tada {{locale}}</p>
         <p i18n>Bienvenue sur notre super site</p>
         <p i18n>Dernière mise à jour effectuée il y a {{lastUpdateDays}} jours</p>
     `,
@@ -13,6 +14,7 @@ export class HomeComponent implements OnInit {
     lastUpdateDays = 2
 
     constructor() {
+    constructor(@Inject(LOCALE_ID) public locale: string) {
     }
 
     ngOnInit(): void {
