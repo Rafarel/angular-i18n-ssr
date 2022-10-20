cf5b1b543f9b09d2d8f891928fbad8a7f501b8ac
Rafarel <rafarel007@hotmail.com>
2022-10-20 14:23:27 +0200

    Implement a factory to build i18n routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,59 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {RouterModule, ROUTES, Routes} from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {ContactComponent} from "./components/contact/contact.component";
+import {I18nService} from "./services/i18n.service";
 
-const routes: Routes = [
-    {path: "", title: "Titre accueil", component: HomeComponent},
-    {path: "contactez-nous", title: "Contactez nous", component: ContactComponent},
+interface LocalizedRouteData {
+    path: string,
+    title: string
+}
+
+export const routesI18n: Routes = [
+    {
+        component: HomeComponent,
+        data: {
+            locales: {
+                fr: {path: '', title: 'Accueil'},
+                en: {path: '', title: 'Home'},
+            }
+        }
+    },
+    {
+        component: ContactComponent,
+        data: {
+            locales: {
+                fr: {path: 'contactez-nous', title: 'Contactez nous'},
+                en: {path: 'contact-us', title: 'Contact us'},
+            }
+        }
+    }
 ];
 
+export function routesFactory()
+{
+    return (i18nService: I18nService): Routes => {
+        return routesI18n.map(route => {
+            const localized = route.data!['locales'][i18nService.locale] as LocalizedRouteData;
+            route = {...route, ...localized}
+            return route;
+        });
+    }
+}
+
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {
+    imports: [RouterModule.forRoot([], {
         initialNavigation: 'enabledBlocking'
     })],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [
+        {
+            provide: ROUTES,
+            useFactory: routesFactory(),
+            deps: [I18nService],
+            multi: true
+        }
+    ]
 })
 export class AppRoutingModule {
 }
